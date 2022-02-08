f35cf79e0f3c2f00529f15e491a52b152104f591
Maximilien Rigaut <max.rigaut@gmail.com>
2022-02-08 18:43:34 +0100

    Fixed the call from __main__

--- src/criteriaSorter/__main__.py
+++ src/criteriaSorter/__main__.py
@@ -4,5 +4,8 @@ import sys
 
 # pragma: nocover  # The main call can't be covered because it will only execute through the CLI
 if __name__ == "__main__":
-    criteriaSorter.main(sys.argv)
+    if len(sys.argv)>1:
+        criteriaSorter.main(sys.argv[1:])
+    else:
+        criteriaSorter.main(("-h",))
     sys.exit(0)
